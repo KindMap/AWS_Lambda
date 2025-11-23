f3cc4ccb128034cc59e57c7f98920cbf7ce7eab9
Joe <jojojo7391@gmail.com>
2025-11-24 00:17:37 +0900

    pk issue corrected

--- lambda_function.py
+++ lambda_function.py
@@ -30,48 +30,56 @@ logger.setLevel(logging.INFO)
 # 'https://apis.data.go.kr/B553766/wksn/{endpoint}?serviceKey={API_KEY}&dataType=JSON'
 # dataType -> JSON으로 고정
 
+
+# mngNo => pk로 세팅하기에 부적절함
+# 모든 테이블이 generated_id를 pk로 사용하도록 수정
+
 API_CONFIG = [
     {
-        # 엘리베이터 조회
         "endpoint": "getWksnElvtr",
         "table": "subway_elevator",
-        "pk": "mng_no",
+        "pk": "generated_id",  # PK 변경
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],  # 유니크 ID 생성 기준
         "mapping": {
-            "mng_no": "mngNo",  # 관리번호
-            "stn_cd": "stnCd",  # 역코드
-            "stn_nm": "stnNm",  # 역명
-            "line_nm": "lineNm",  # 호선명
-            "fclt_nm": "fcltNm",  # 시설명
-            "oprtng_situ": "oprtngSitu",  # 가동현황
-            "dtl_pstn": "dtlPstn",  # 상세위치
-            "pscp_nope": "pscpNope",  # 정원인원 수
-            "crtr_ymd": "crtrYmd",  # 기준일자
-        },
-    },
-    {
-        # 장애인화장실 조회
-        "endpoint": "getWksnRstrm",
-        "table": "subway_toilet",
-        "pk": "mng_no",
-        "mapping": {
+            "generated_id": "generated_id",
             "mng_no": "mngNo",
             "stn_cd": "stnCd",
             "stn_nm": "stnNm",
             "line_nm": "lineNm",
             "fclt_nm": "fcltNm",
-            "whlchr_acs_yn": "whlchrAcsPsbltyYn",  # 휠체어 접근 가능 여부
-            "gate_inout": "gateInoutSe",  # 게이트 내외 구분(외부/내부)
-            "rstrm_info": "rstrmInfo",  # 화장실 구분 상세
-            "stn_flr": "stnFlr",  # 위치층
+            "oprtng_situ": "oprtngSitu",
+            "dtl_pstn": "dtlPstn",
+            "pscp_nope": "pscpNope",
+            "crtr_ymd": "crtrYmd",
+            "elvtr_sn": "elvtrSn",
+        },
+    },
+    {
+        "endpoint": "getWksnRstrm",
+        "table": "subway_toilet",
+        "pk": "generated_id",
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],
+        "mapping": {
+            "generated_id": "generated_id",
+            "mng_no": "mngNo",
+            "stn_cd": "stnCd",
+            "stn_nm": "stnNm",
+            "line_nm": "lineNm",
+            "fclt_nm": "fcltNm",
+            "whlchr_acs_yn": "whlchrAcsPsbltyYn",
+            "gate_inout": "gateInoutSe",
+            "rstrm_info": "rstrmInfo",
+            "stn_flr": "stnFlr",
             "crtr_ymd": "crtrYmd",
         },
     },
     {
-        # 휠체어리프트 조회
         "endpoint": "getWksnWhcllift",
         "table": "subway_lift",
-        "pk": "mng_no",
+        "pk": "generated_id",
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],
         "mapping": {
+            "generated_id": "generated_id",
             "mng_no": "mngNo",
             "stn_cd": "stnCd",
             "stn_nm": "stnNm",
@@ -79,18 +87,19 @@ API_CONFIG = [
             "fclt_nm": "fcltNm",
             "oprtng_situ": "oprtngSitu",
             "limit_wht": "limitWht",
-            "bgng_flr_dtl": "bgngFlrDtlPstn",  # 시작층 상세위치
-            "end_flr_dtl": "endFlrDtlPstn",  # 종료층 상세위치
+            "bgng_flr_dtl": "bgngFlrDtlPstn",
+            "end_flr_dtl": "endFlrDtlPstn",
             "crtr_ymd": "crtrYmd",
-            "vcnt_entrc_no": "vcntEntrcNo",  # 근접 출입구 위치 => db 추가하기
+            "vcnt_entrc_no": "vcntEntrcNo",
         },
     },
     {
-        # 무빙워크 조회 데이터
         "endpoint": "getWksnMvnwlk",
         "table": "subway_movingwalk",
-        "pk": "mng_no",
+        "pk": "generated_id",
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],
         "mapping": {
+            "generated_id": "generated_id",
             "mng_no": "mngNo",
             "stn_cd": "stnCd",
             "stn_nm": "stnNm",
@@ -98,82 +107,247 @@ API_CONFIG = [
             "fclt_nm": "fcltNm",
             "oprtng_situ": "oprtngSitu",
             "crtr_ymd": "crtrYmd",
-            "vcnt_entrc_no": "vcntEntrcNo",  # 근접 출입구 위치 => db 추가하기
+            "vcnt_entrc_no": "vcntEntrcNo",
         },
     },
     {
-        # 휠체어 급속 충전기 조회
-        # => mngNo가 '-'로 들어오는 경우가 있어 generated_id 사용
         "endpoint": "getWksnWhclCharge",
         "table": "subway_charger",
         "pk": "generated_id",
-        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],  # 고유 키 조합
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],
         "mapping": {
+            "generated_id": "generated_id",
             "mng_no": "mngNo",
             "stn_cd": "stnCd",
             "stn_nm": "stnNm",
             "line_nm": "lineNm",
             "fclt_nm": "fcltNm",
             "dtl_pstn": "dtlPstn",
-            "cnnctr_se": "cnnctrSe",  # 커넥터 구분
-            "elctc_fac_cnt": "elctcFacCnt",  # 충전설비수
-            "utztn_crg": "utztnCrg",  # 이용요금
-            "oper_tel": "operInstTelno",  # 운영기관 전화번호
+            "cnnctr_se": "cnnctrSe",
+            "elctc_fac_cnt": "elctcFacCnt",
+            "utztn_crg": "utztnCrg",
+            "oper_tel": "operInstTelno",
             "crtr_ymd": "crtrYmd",
         },
     },
     {
-        # 수어 영상 전화기 조회
         "endpoint": "getWksnSlng",
         "table": "subway_sign_phone",
-        "pk": "generated_id",  # 생성된 ID를 PK로 사용
-        "pk_gen_keys": ["stnCd", "lineNm", "dtlPstn"],  # 유니크 조합 키
+        "pk": "generated_id",
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],
         "mapping": {
-            "generated_id": "generated_id",  # 매핑에도 추가
+            "generated_id": "generated_id",
             "stn_cd": "stnCd",
             "stn_nm": "stnNm",
             "line_nm": "lineNm",
             "fclt_nm": "fcltNm",
             "dtl_pstn": "dtlPstn",
-            "vcnt_entrc_no": "vcntEntrcNo",  # 근접 출입구 위치
+            "vcnt_entrc_no": "vcntEntrcNo",
             "stn_flr": "stnFlr",
-            "utztn_hr": "utztnHr",  # 이용 시간
+            "utztn_hr": "utztnHr",
             "crtr_ymd": "crtrYmd",
         },
     },
     {
-        # 안전발판 보유현황 조회
         "endpoint": "getWksnSafePlfm",
         "table": "subway_safe_platform",
         "pk": "generated_id",
-        "pk_gen_keys": ["stnCd", "lineNm"],  # 역+노선 기준으로 하나씩 있다고 가정
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],
         "mapping": {
             "generated_id": "generated_id",
             "stn_cd": "stnCd",
             "stn_nm": "stnNm",
             "line_nm": "lineNm",
             "fclt_nm": "fcltNm",
-            "sfty_scf_yn": "sftyScfldEn",  # 안전발판유무
-            "mngr_tel": "mngrTelno",  # 관리자 전화번호
+            "sfty_scf_yn": "sftyScfldEn",
+            "mngr_tel": "mngrTelno",
             "crtr_ymd": "crtrYmd",
         },
     },
     {
-        # 교통약자 도우미 현황 조회
         "endpoint": "getWksnHelper",
         "table": "subway_helper",
         "pk": "generated_id",
-        "pk_gen_keys": ["stnCd", "lineNm"],
+        "pk_gen_keys": [
+            "stnCd",
+            "lineNm",
+            "stnNm",
+        ],  # 도우미는 시설명이 없으므로 역명 사용
         "mapping": {
             "generated_id": "generated_id",
             "stn_cd": "stnCd",
             "stn_nm": "stnNm",
             "line_nm": "lineNm",
-            "helper_tel": "trffcWksnHlprTelno",  # 해당 교통약자도우미 전화번호
+            "helper_tel": "trffcWksnHlprTelno",
+            "crtr_ymd": "crtrYmd",
+        },
+    },
+    {
+        "endpoint": "getWksnEsctr",
+        "table": "subway_escalator",
+        "pk": "generated_id",
+        "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],  # 고유 ID 생성 기준
+        "mapping": {
+            "generated_id": "generated_id",
+            "mng_no": "mngNo",
+            "stn_cd": "stnCd",
+            "stn_nm": "stnNm",
+            "line_nm": "lineNm",
+            "fclt_nm": "fcltNm",
+            "oprtng_situ": "oprtngSitu",
+            "upbdnb_se": "upbdnbSe",  # 상하행 구분
+            "bgng_flr_dtl": "bgngFlrDtlPstn",  # 시작층 상세위치
+            "end_flr_dtl": "endFlrDtlPstn",  # 종료층 상세위치
+            "vcnt_entrc_no": "vcntEntrcNo",  # 근접 출입구 번호
             "crtr_ymd": "crtrYmd",
         },
     },
 ]
+
+# API_CONFIG = [
+#     {
+#         # 엘리베이터 조회
+#         "endpoint": "getWksnElvtr",
+#         "table": "subway_elevator",
+#         "pk": "mng_no",
+#         "mapping": {
+#             "mng_no": "mngNo",  # 관리번호
+#             "stn_cd": "stnCd",  # 역코드
+#             "stn_nm": "stnNm",  # 역명
+#             "line_nm": "lineNm",  # 호선명
+#             "fclt_nm": "fcltNm",  # 시설명
+#             "oprtng_situ": "oprtngSitu",  # 가동현황
+#             "dtl_pstn": "dtlPstn",  # 상세위치
+#             "pscp_nope": "pscpNope",  # 정원인원 수
+#             "crtr_ymd": "crtrYmd",  # 기준일자
+#         },
+#     },
+#     {
+#         # 장애인화장실 조회
+#         "endpoint": "getWksnRstrm",
+#         "table": "subway_toilet",
+#         "pk": "mng_no",
+#         "mapping": {
+#             "mng_no": "mngNo",
+#             "stn_cd": "stnCd",
+#             "stn_nm": "stnNm",
+#             "line_nm": "lineNm",
+#             "fclt_nm": "fcltNm",
+#             "whlchr_acs_yn": "whlchrAcsPsbltyYn",  # 휠체어 접근 가능 여부
+#             "gate_inout": "gateInoutSe",  # 게이트 내외 구분(외부/내부)
+#             "rstrm_info": "rstrmInfo",  # 화장실 구분 상세
+#             "stn_flr": "stnFlr",  # 위치층
+#             "crtr_ymd": "crtrYmd",
+#         },
+#     },
+#     {
+#         # 휠체어리프트 조회
+#         "endpoint": "getWksnWhcllift",
+#         "table": "subway_lift",
+#         "pk": "mng_no",
+#         "mapping": {
+#             "mng_no": "mngNo",
+#             "stn_cd": "stnCd",
+#             "stn_nm": "stnNm",
+#             "line_nm": "lineNm",
+#             "fclt_nm": "fcltNm",
+#             "oprtng_situ": "oprtngSitu",
+#             "limit_wht": "limitWht",
+#             "bgng_flr_dtl": "bgngFlrDtlPstn",  # 시작층 상세위치
+#             "end_flr_dtl": "endFlrDtlPstn",  # 종료층 상세위치
+#             "crtr_ymd": "crtrYmd",
+#             "vcnt_entrc_no": "vcntEntrcNo",  # 근접 출입구 위치 => db 추가하기
+#         },
+#     },
+#     {
+#         # 무빙워크 조회 데이터
+#         "endpoint": "getWksnMvnwlk",
+#         "table": "subway_movingwalk",
+#         "pk": "mng_no",
+#         "mapping": {
+#             "mng_no": "mngNo",
+#             "stn_cd": "stnCd",
+#             "stn_nm": "stnNm",
+#             "line_nm": "lineNm",
+#             "fclt_nm": "fcltNm",
+#             "oprtng_situ": "oprtngSitu",
+#             "crtr_ymd": "crtrYmd",
+#             "vcnt_entrc_no": "vcntEntrcNo",  # 근접 출입구 위치 => db 추가하기
+#         },
+#     },
+#     {
+#         # 휠체어 급속 충전기 조회
+#         # => mngNo가 '-'로 들어오는 경우가 있어 generated_id 사용
+#         "endpoint": "getWksnWhclCharge",
+#         "table": "subway_charger",
+#         "pk": "generated_id",
+#         "pk_gen_keys": ["stnCd", "lineNm", "fcltNm"],  # 고유 키 조합
+#         "mapping": {
+#             "mng_no": "mngNo",
+#             "stn_cd": "stnCd",
+#             "stn_nm": "stnNm",
+#             "line_nm": "lineNm",
+#             "fclt_nm": "fcltNm",
+#             "dtl_pstn": "dtlPstn",
+#             "cnnctr_se": "cnnctrSe",  # 커넥터 구분
+#             "elctc_fac_cnt": "elctcFacCnt",  # 충전설비수
+#             "utztn_crg": "utztnCrg",  # 이용요금
+#             "oper_tel": "operInstTelno",  # 운영기관 전화번호
+#             "crtr_ymd": "crtrYmd",
+#         },
+#     },
+#     {
+#         # 수어 영상 전화기 조회
+#         "endpoint": "getWksnSlng",
+#         "table": "subway_sign_phone",
+#         "pk": "generated_id",  # 생성된 ID를 PK로 사용
+#         "pk_gen_keys": ["stnCd", "lineNm", "dtlPstn"],  # 유니크 조합 키
+#         "mapping": {
+#             "generated_id": "generated_id",  # 매핑에도 추가
+#             "stn_cd": "stnCd",
+#             "stn_nm": "stnNm",
+#             "line_nm": "lineNm",
+#             "fclt_nm": "fcltNm",
+#             "dtl_pstn": "dtlPstn",
+#             "vcnt_entrc_no": "vcntEntrcNo",  # 근접 출입구 위치
+#             "stn_flr": "stnFlr",
+#             "utztn_hr": "utztnHr",  # 이용 시간
+#             "crtr_ymd": "crtrYmd",
+#         },
+#     },
+#     {
+#         # 안전발판 보유현황 조회
+#         "endpoint": "getWksnSafePlfm",
+#         "table": "subway_safe_platform",
+#         "pk": "generated_id",
+#         "pk_gen_keys": ["stnCd", "lineNm"],  # 역+노선 기준으로 하나씩 있다고 가정
+#         "mapping": {
+#             "generated_id": "generated_id",
+#             "stn_cd": "stnCd",
+#             "stn_nm": "stnNm",
+#             "line_nm": "lineNm",
+#             "fclt_nm": "fcltNm",
+#             "sfty_scf_yn": "sftyScfldEn",  # 안전발판유무
+#             "mngr_tel": "mngrTelno",  # 관리자 전화번호
+#             "crtr_ymd": "crtrYmd",
+#         },
+#     },
+#     {
+#         # 교통약자 도우미 현황 조회
+#         "endpoint": "getWksnHelper",
+#         "table": "subway_helper",
+#         "pk": "generated_id",
+#         "pk_gen_keys": ["stnCd", "lineNm"],
+#         "mapping": {
+#             "generated_id": "generated_id",
+#             "stn_cd": "stnCd",
+#             "stn_nm": "stnNm",
+#             "line_nm": "lineNm",
+#             "helper_tel": "trffcWksnHlprTelno",  # 해당 교통약자도우미 전화번호
+#             "crtr_ymd": "crtrYmd",
+#         },
+#     },
+# ]
 
 
 def get_connection():
